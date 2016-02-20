b24f3612f46c037cd9ed4ed3ecfc09632575d40c
KMAKFRSW <kimiakifurusawa@gmail.com>
2016-02-21 02:29:16 +0900

    initial data

--- lib/tasks/calc_initialdata.rb
+++ lib/tasks/calc_initialdata.rb
@@ -5,70 +5,8 @@ require "#{Rails.root}/app/models/fx_rate"
 require "#{Rails.root}/app/models/fx_performance"
 include Calculation
 
-class Tasks::Calculate_InitialData
-  def self.execute
-    ##############################################################
-    # calc following performance items :                         #
-    #     average of daily range for each terms                  #
-    # scope of currency :                                        #
-    #     'USD/JPY', 'EUR/JPY','EUR/USD'                         #
-    ##############################################################
-
-    # define each item codes
-    item_avg_range_5d     = Settings[:item][:range_5d_avg]
-    item_avg_range_25d    = Settings[:item][:range_25d_avg]
-    item_avg_range_75d    = Settings[:item][:range_75d_avg]
-    item_avg_range_100d   = Settings[:item][:range_100d_avg]
-
-    # get reference date (format:YYYYMMDD)
-    yesterday = (Date.today - 1).strftime("%Y%m%d")
-
-    for num in 2..360 do
-      yesterday = (Date.today - num).strftime("%Y%m%d")
-      weekday = (Date.today - num).wday 
-      if weekday == 1 || weekday == 2 || weekday == 3 || weekday == 4 || weekday == 5 then
-        
-        # define array for loop procedure
-        arr_calc_target = [
-          [5,item_avg_range_5d],
-          [25,item_avg_range_25d],
-          [75,item_avg_range_75d],
-          [100,item_avg_range_100d]
-        ]
-              
-        # declare some arrays for each terms
-        array_avg_range = Array.new
-    
-        # calculate avg of daily range for past 5 day
-        arr_calc_target.each do |term, item_code|
-          array_avg_range = FxPerformance.find_by_sql(["select cur_code, round(avg(data), 3) as avg from fx_performances
-          where calc_date between date_format( ? - INTERVAL ? DAY,'%Y%m%d')  and ?
-          and cur_code IN ( ?, ?, ?, ?, ?, ?, ?)
-          and item = 'RNG01'
-          group by cur_code
-          order by cur_code asc
-          ", yesterday, term, yesterday,'USD/JPY','EUR/JPY','EUR/USD','AUD/JPY','ADU/USD','GBP/JPY','GBP/USD'])
-          
-          array_avg_range.each do |row|
-            if FxPerformance.exists?({ :cur_code => row.cur_code, :calc_date => yesterday, :item => item_code })
-                @FxPerformance = FxPerformance.find_by_cur_code_and_calc_date_and_item(row.cur_code, yesterday, item_code)
-                @FxPerformance.attributes = {
-                    :data => row.avg
-                }
-                @FxPerformance.save!
-            else
-              FxPerformance.create!(
-                  :cur_code  => row.cur_code,
-                  :calc_date => yesterday,
-                  :item      => item_code,
-                  :data      => row.avg
-                  )
-            end
-          end
-        end
-      end
-    end
-  
+class Tasks::InitialData
+  def self.calc_setup
     ##############################################################
     # calc following performance items :                         #
     #     Daily Range                                            #
@@ -94,7 +32,7 @@ class Tasks::Calculate_InitialData
         ranges = FxRate.find_by_sql(["select trade_date, product_code2, format(round(high_price - low_price, 3),3) as 'range' from fx_rates 
         where trade_date = ?
         and product_code2 IN ( ?, ?, ?, ?, ?, ?, ?)
-        ", yesterday, 'USD/JPY','EUR/JPY','EUR/USD','AUD/JPY','ADU/USD','GBP/JPY','GBP/USD'])
+        ", yesterday, 'USD/JPY','EUR/JPY','EUR/USD','AUD/JPY','AUD/USD','GBP/JPY','GBP/USD'])
         
         ranges.each do |row|
           if FxPerformance.exists?({ :cur_code => row.product_code2, :calc_date => row.trade_date, :item => item_range })
@@ -114,6 +52,69 @@ class Tasks::Calculate_InitialData
         end
       end
     end  
+
+    ##############################################################
+    # calc following performance items :                         #
+    #     average of daily range for each terms                  #
+    # scope of currency :                                        #
+    #     'USD/JPY', 'EUR/JPY','EUR/USD'                         #
+    ##############################################################
+
+    # define each item codes
+    item_avg_range_5d     = Settings[:item][:range_5d_avg]
+    item_avg_range_25d    = Settings[:item][:range_25d_avg]
+    item_avg_range_75d    = Settings[:item][:range_75d_avg]
+    item_avg_range_100d   = Settings[:item][:range_100d_avg]
+
+    # get reference date (format:YYYYMMDD)
+    yesterday = (Date.today - 1).strftime("%Y%m%d")
+
+    for num in 2..360 do
+      yesterday = (Date.today - num).strftime("%Y%m%d")
+      weekday = (Date.today - num).wday
+      if weekday == 1 || weekday == 2 || weekday == 3 || weekday == 4 || weekday == 5 then
+        
+        # define array for loop procedure
+        arr_calc_target = [
+          [5,item_avg_range_5d],
+          [25,item_avg_range_25d],
+          [75,item_avg_range_75d],
+          [100,item_avg_range_100d]
+        ]
+              
+        # declare some arrays for each terms
+        array_avg_range = Array.new
+    
+        # calculate avg of daily range for past 5 day
+        arr_calc_target.each do |term, item_code|
+          array_avg_range = FxPerformance.find_by_sql(["select cur_code, round(avg(data), 3) as avg from fx_performances
+          where calc_date between date_format( ? - INTERVAL ? DAY,'%Y%m%d')  and ?
+          and cur_code IN ( ?, ?, ?, ?, ?, ?, ?)
+          and item = 'RNG01'
+          group by cur_code
+          order by cur_code asc
+          ", yesterday, term, yesterday,'USD/JPY','EUR/JPY','EUR/USD','AUD/JPY','AUD/USD','GBP/JPY','GBP/USD'])
+          
+          array_avg_range.each do |row|
+            if FxPerformance.exists?({ :cur_code => row.cur_code, :calc_date => yesterday, :item => item_code })
+                @FxPerformance = FxPerformance.find_by_cur_code_and_calc_date_and_item(row.cur_code, yesterday, item_code)
+                @FxPerformance.attributes = {
+                    :data => row.avg
+                }
+                @FxPerformance.save!
+            else
+              FxPerformance.create!(
+                  :cur_code  => row.cur_code,
+                  :calc_date => yesterday,
+                  :item      => item_code,
+                  :data      => row.avg
+                  )
+            end
+          end
+        end
+      end
+    end
+  
   
     ##############################################################
     # calc following performance items :                         #
@@ -140,7 +141,7 @@ class Tasks::Calculate_InitialData
         prev_rates = FxRate.find_by_sql(["select trade_date, product_code2, round((close_price / prev_price -1) , 3) as 'prev_rate' from fx_rates 
         where trade_date = ?
         and product_code2 IN ( ?, ?, ?, ?, ?, ?, ?)
-        ", yesterday,'USD/JPY','EUR/JPY','EUR/USD','AUD/JPY','ADU/USD','GBP/JPY','GBP/USD'])
+        ", yesterday,'USD/JPY','EUR/JPY','EUR/USD','AUD/JPY','AUD/USD','GBP/JPY','GBP/USD'])
         
         prev_rates.each do |row|
           if FxPerformance.exists?({ :cur_code => row.product_code2, :calc_date => row.trade_date, :item => item_daily_return })
@@ -199,9 +200,9 @@ class Tasks::Calculate_InitialData
         # get max 60 months rates and store them into array sorting product_code2 and trade_date order
         max_60m_return = FxRate.find_by_sql(["select trade_date as calc_date, product_code2 as cur_code, (((close_price / prev_price) -1) * 100) as data from fx_rates 
         where trade_date between date_format( ? - INTERVAL 5 YEAR,'%Y%m%d') and ? 
-        and product_code2 IN ( ?, ?, ?)
+        and product_code2 IN ( ?, ?, ? ?, ? ?, ?)
         order by cur_code asc, calc_date desc
-        ", yesterday, yesterday, 'USD/JPY','EUR/JPY','EUR/USD','AUD/JPY','ADU/USD','GBP/JPY','GBP/USD'])
+        ", yesterday, yesterday, 'USD/JPY','EUR/JPY','EUR/USD','AUD/JPY','AUD/USD','GBP/JPY','GBP/USD'])
         
         # initialize
         wk_cur_code = nil
