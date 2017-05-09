4f27dc57b983adc77685f4149cd94e368f12eabc
KMAKFRSW <kimiakifurusawa@gmail.com>
2017-05-09 23:58:04 +0900

    modify http access

--- lib/tasks/tool/http_client.rb
+++ lib/tasks/tool/http_client.rb
@@ -1,12 +1,13 @@
 #encoding: utf-8
 
 require 'open-uri'
+require 'open_uri_redirections'
 require 'nokogiri'
 
 module HttpClient
   def self.get(url)
     begin
-      html = open(url).read
+      html = open(url, :allow_redirections => :safe).read
     rescue URI::InvalidURIError
       url = URI.encode(url)
       html = open(url, "Accept-Encoding" => "utf-8")
