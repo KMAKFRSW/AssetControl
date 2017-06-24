15e6b3f69a19e2da19ac2a8c788577fe634339bf
KMAKFRSW <kimiakifurusawa@gmail.com>
2017-06-24 17:44:22 +0900

    add tryjpy

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,11 @@
 #MarketData.create(:security_code => 'EURUSD', :vendor_code => '01', :market_code => 'DMY', :from_date => 20160125, :to_date => 99991231, :asset_class =>'FX', :region_code => 'DMY', :security_name => 'ユーロドル')
 #MarketData.create(:security_code => 'AUDUSD', :vendor_code => '01', :market_code => 'DMY', :from_date => 20160125, :to_date => 99991231, :asset_class =>'FX', :region_code => 'DMY', :security_name => '豪ドルドル')
 #MarketData.create(:security_code => 'GBPUSD', :vendor_code => '01', :market_code => 'DMY', :from_date => 20160125, :to_date => 99991231, :asset_class =>'FX', :region_code => 'DMY', :security_name => 'ポンドドル')
-MarketData.create(:security_code => 'NZDJPY', :vendor_code => '01', :market_code => 'DMY', :from_date => 20160326, :to_date => 99991231, :asset_class =>'FX', :region_code => 'DMY', :security_name => 'ニュージーランド円')
-MarketData.create(:security_code => 'CADJPY', :vendor_code => '01', :market_code => 'DMY', :from_date => 20160326, :to_date => 99991231, :asset_class =>'FX', :region_code => 'DMY', :security_name => 'カナダドル円')
+#MarketData.create(:security_code => 'NZDJPY', :vendor_code => '01', :market_code => 'DMY', :from_date => 20160326, :to_date => 99991231, :asset_class =>'FX', :region_code => 'DMY', :security_name => 'ニュージーランド円')
+#MarketData.create(:security_code => 'CADJPY', :vendor_code => '01', :market_code => 'DMY', :from_date => 20160326, :to_date => 99991231, :asset_class =>'FX', :region_code => 'DMY', :security_name => 'カナダドル円')
+
+require "csv"
+
+CSV.foreach('db/TRYJPY.csv') do |row|
+  FxRate.create!(trade_date: row[0], open_price: row[1], high_price: row[2], low_price: row[3], close_price: row[4],prev_price: row[5], product_code1: '01', product_code2: 'TRY/JPY', product_name: 'トルコリラ', data_kbn: 'D01')
+end
